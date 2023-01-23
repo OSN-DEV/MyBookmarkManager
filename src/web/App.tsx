e934d621505f62866c89e3e8d14074cb31c0a18f
OSN-DEV <osndev1973@gmail.com>
2023-01-23 21:30:18 +0900

    create skelton

--- src/web/App.tsx
+++ src/web/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import './App.css';
+// import './App.css';      ここと次の行の差し替えで、output.cssがApp.cssとして出力される
+import './styles/output.css';
 import Pattern1 from './Pattern1'
 import Pattern2 from './Pattern2'
 import Pattern3 from './Pattern3';
@@ -7,9 +8,12 @@ import Pattern3 from './Pattern3';
 export const App = () => {
   return (
     <div className="container">
-      <Pattern1 />
-      <Pattern2 />
-      <Pattern3 />
+<div className="flex">
+  <div className="bg-green-100 px-2">1</div>
+  <div className="bg-green-100 px-2 ml-1">2</div>
+  <div className="bg-green-100 px-2 ml-1">3</div>
+</div>
+
     </div>
   );
 };
